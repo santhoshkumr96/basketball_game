3adb5192d388b4a36a7de9209e175a3b68f8d1b7
Santhosh Subramaniam <santhoshkumr96@gmail.com>
2019-12-29 09:32:11 +0530

    prototype

---
+++ ScoreScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 1);
+        print(PlayerPrefs.GetInt("score"));
+    }
+}
---
+++ SwipeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeScript : MonoBehaviour
+{
+
+    Vector2 startPos, endPos, direction;
+    float startTime, endTime, timeInterval;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTime = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTime = Time.time;
+            timeInterval = startTime - endTime;
+            endPos = Input.GetTouch(0).position;
+            direction = startPos - endPos;
+
+            GetComponent<Rigidbody2D>().AddForce(direction/timeInterval * 0.15f); 
+        }
+
+    }
+}
